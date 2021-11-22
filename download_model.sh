6c09b071d0604a98e793c110ffd60d8da704b204
Pascal Beaulne <pascalbeaulne@Pascals-MacBook-Pro.local>
2021-11-22 15:54:16 -0500

    Added az configure

--- download_model.sh
+++ download_model.sh
@@ -1,5 +1,6 @@
 # Login to Azure with a Service Principal
 az login --service-principal -u $SPN_ID -p $SPN_PWD --tenant $TENANT_ID
+az extension add -n azure-cli-ml 
 az configure --defaults group=$RESOURCE_GROUP
 
 # Get Model ID
