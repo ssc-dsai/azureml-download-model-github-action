017c2554fda810e5ccdd7c2aa1c23ebf0e47500b
Pascal Beaulne <pascalbeaulne@Pascals-MacBook-Pro.local>
2021-11-22 15:30:02 -0500

    Added ml extension

--- download_model.sh
+++ download_model.sh
@@ -1,6 +1,9 @@
 # Login to Azure with a Service Principal
 az login --service-principal -u $SPN_ID -p $SPN_PWD --tenant $TENANT_ID
 
+# add Azure ML extention
+az extension add azure-cli-ml
+
 # Get Model ID
 LIST=$(az ml model list --model-name $MODEL_NAME --workspace-name $WORKSPACE_NAME)
 
