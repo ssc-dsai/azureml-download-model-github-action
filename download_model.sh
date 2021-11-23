0ee434576612c5b650fc64f75ca8288a3db25af3
Pascal Beaulne <pascalbeaulne@Pascals-MacBook-Pro.local>
2021-11-23 11:29:24 -0500

    changed azure login code

--- download_model.sh
+++ download_model.sh
@@ -1,10 +1,4 @@
-# Add Azure ML CLi
-# az extension add -n azure-cli-ml
-
-# Add the resource group in the configuration
-# az configure --defaults group=$RESOURCE_GROUP
-
-# Get last model
+# Get latest model
 MODEL=$(az ml model list --model-name $MODEL_NAME --workspace-name $WORKSPACE_NAME | jq '.[-1].id')
 
 echo $MODEL
