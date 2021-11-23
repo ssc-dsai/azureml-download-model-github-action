c30a6eb8767d7184d4d2ef2a3782426cb3725abb
Pascal Beaulne <pascalbeaulne@Pascals-MacBook-Pro.local>
2021-11-23 12:34:06 -0500

    Switched to Python approach

---
+++ download_model.py
@@ -0,0 +1,38 @@
+from azureml.core import Workspace
+from azureml.core.authentication import ServicePrincipalAuthentication
+from azureml.core.model import Model
+import os
+
+# Load .env variables
+CONFIG = os.getenv('CONFIG')
+MODEL_NAME = os.getenv('MODEL_NAME')
+SPN_ID = os.getenv('SPN_ID')
+SPN_PWD = os.getenv('SPN_PWD')
+TENANT_ID = os.getenv('TENANT_ID')
+
+def main():
+    auth = ServicePrincipalAuthentication(
+        tenant_id=TENANT_ID, 
+        service_principal_id=SPN_ID, 
+        service_principal_password=SPN_PWD
+    )
+
+    ws = Workspace(subscription_id = CONFIG["subscription_id"], resource_group = CONFIG["resource_group"], workspace_name = CONFIG["workspace_name"], auth=auth)
+
+    # ws = Workspace.from_config(path='config.json', auth=auth)
+
+    #model = Model(ws, name=MODEL_NAME)
+
+    # print('Model found. Downloading (this may take a while...)')
+    # model.download(target_dir='.', exist_ok=True)
+
+    # # Rename the retrieved file to the path expected by API
+    # model_path = Model.get_model_path(model_name=MODEL_NAME, _workspace=ws)
+    # print('Success. Model downloaded to:')
+    # print(model_path)
+    # os.rename(model_path, 'api/trained_model.bin')
+    # print('Model moved to api/trained_model.bin')
+
+
+if __name__ == "__main__":
+    main()
--- download_model.sh
+++
@@ -1,4 +0,0 @@
-# Get latest model
-MODEL=$(az ml model list --model-name $MODEL_NAME --workspace-name $WORKSPACE_NAME | jq '.[-1].id')
-
-echo $MODEL
