f42462155aeafaad7f59f34205a9c5aabfb38b9b
Pascal Beaulne <pascalbeaulne@Pascals-MacBook-Pro.local>
2021-11-23 10:51:48 -0500

    Using azure cli actions

--- download_model.sh
+++ download_model.sh
@@ -1,12 +1,8 @@
 # Add Azure ML CLi
-az extension add -n azure-cli-ml
+# az extension add -n azure-cli-ml
 
 # Add the resource group in the configuration
-az configure --defaults group=$RESOURCE_GROUP
-
-# Login to Azure with a Service Principal
-az login --service-principal -u $SPN_ID -p $SPN_PWD --tenant $TENANT_ID
-
+# az configure --defaults group=$RESOURCE_GROUP
 
 # Get last model
 MODEL=$(az ml model list --model-name $MODEL_NAME --workspace-name $WORKSPACE_NAME | jq '.[-1].id')
