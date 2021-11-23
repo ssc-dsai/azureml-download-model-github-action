d886f31834a22dc9b6e042ad537cccc48038d06e
Pascal Beaulne <pascalbeaulne@Pascals-MacBook-Pro.local>
2021-11-23 10:13:18 -0500

    filtering last model

--- download_model.sh
+++ download_model.sh
@@ -1,9 +1,14 @@
-# Login to Azure with a Service Principal
-az login --service-principal -u $SPN_ID -p $SPN_PWD --tenant $TENANT_ID
-az extension add -n azure-cli-ml 
+# Add Azure ML CLi
+az extension add -n azure-cli-ml
+
+# Add the resource group in the configuration
 az configure --defaults group=$RESOURCE_GROUP
 
-# Get Model ID
-LIST=$(az ml model list --model-name $MODEL_NAME --workspace-name $WORKSPACE_NAME)
+# Login to Azure with a Service Principal
+az login --service-principal -u $SPN_ID -p $SPN_PWD --tenant $TENANT_ID
 
-echo LIST
+
+# Get last model
+MODEL=$(az ml model list --model-name $MODEL_NAME --workspace-name $WORKSPACE_NAME | jq '.[-1].id')
+
+echo $MODEL
